40c9c2cb1feac2deee13fb153b32ef11cc9b366a
Christiano0407 <placebo0704@gmail.com>
2021-11-16 22:53:27 -0600

    add data innerHTML

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,21 @@
 const loadMovies = async () => {
   try {
     const request = await fetch(
-      `https://api.themoviedb.org/3/movie/550?api_key=c66eb9e2b42b5d1d179fff7ac34ce71f&language=es-MX`
+      `https://api.themoviedb.org/3/movie/popular?api_key=c66eb9e2b42b5d1d179fff7ac34ce71f&language=es-MX`
     );
     console.log(request);
     //> Comprobar => Estado
     if (request.status === 200) {
       //> Data
       const data = await request.json();
-      console.log(data);
+
+      let movies = " ";
+      //>
+      data.results.forEach((movie) => {
+        movies += `<h1>${movie.title}</h1>`;
+      });
+      document.getElementById(`container`).innerHTML = movies;
+
       //console.log(data.title); // properties>
     } else if (request.status === 401) {
       console.log("Key is wrong");
