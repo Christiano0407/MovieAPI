77447dfb5ac6a9a8cd9627150c4e0f8ae539b041
Christiano0407 <placebo0704@gmail.com>
2021-11-16 22:02:36 -0600

    request_Movies

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 // API Javascript>
+// (A)> Cargar películas>>
+const loadMovies = async () => {
+  try {
+    const request = await fetch(
+      `https://api.themoviedb.org/3/movie/550?api_key=c66eb9e2b42b5d1d179fff7ac34ce71f&language=es-MX`
+    );
+    console.log(request);
+    //> Data
+    const data = await request.json();
+    console.log(data);
+    console.log(data.title);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+loadMovies();
