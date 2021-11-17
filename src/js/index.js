4ea3848d97c255f5cea01333da0385f5d6c6bd95
Christiano0407 <placebo0704@gmail.com>
2021-11-17 15:33:06 -0600

    vídeos/observer

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,25 @@
 // API Javascript>
 //> Pagination
 let pages = 1;
+let movies = " ";
+let endMovie;
+// << Intersection Observer / Scroll Infinito >>>
+let observer = new IntersectionObserver(
+  (entries, observer) => {
+    console.log(entries);
+    entries.forEach((entries) => {
+      if (entries.isIntersecting) {
+        pages++;
+        loadMovies();
+      }
+    });
+  },
+  {
+    rootMargin: `0px 0px 200px 0px`,
+    threshold: 1.0,
+  }
+);
+// ==================================================================
 //const btnBefore = document.getElementById(`btnBefore`);
 //const btnAfter = document.getElementById(`btnAfter`);
 
@@ -18,19 +37,19 @@ let pages = 1;
 /*   } */
 /* }); */
 
-// (A)> Cargar películas>>>>>>>>>>>>>>
+// (A)> Cargar películas>>>>>>>>> =================================== >>>>>
 const loadMovies = async () => {
   try {
     const request = await fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=c66eb9e2b42b5d1d179fff7ac34ce71f&language=es-MX&page=${pages}`
     );
-    console.log(request);
+    //console.log(request);
     //> Comprobar => Estado
     if (request.status === 200) {
       //> Data
       const data = await request.json();
-      //>
-      let movies = " ";
+      //> arriba =>
+      //let movies = " ";
       //>
       data.results.forEach((movie) => {
         movies += ` 
@@ -40,7 +59,23 @@ const loadMovies = async () => {
          </div>
          `;
       });
+
       document.getElementById(`container`).innerHTML = movies;
+      //> Lllega al máximo==>
+      if (pages < 1000) {
+        if (endMovie) {
+          observer.unobserve(endMovie);
+        }
+
+        //> Comprobar todas las pelis, que van a aparecer (Observer) >
+        const moviesInScreen = document.querySelectorAll(`.movie`);
+        //console.log(moviesInScreen);
+        //let endMovie = moviesInScreen[moviesInScreen.length - 1];
+        endMovie = moviesInScreen[moviesInScreen.length - 1];
+        console.log(endMovie);
+        //> Observer
+        observer.observe(endMovie);
+      }
 
       //console.log(data.title); // properties>
     } else if (request.status === 401) {
