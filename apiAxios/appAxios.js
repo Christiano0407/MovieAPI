f0f3a8128dc9e203cf47e30a2c48e6333a7e35d1
Christiano0407 <placebo0704@gmail.com>
2021-11-21 17:30:44 -0600

    axios-2

--- apiAxios/appAxios.js
+++ apiAxios/appAxios.js
@@ -1,4 +1,38 @@
 console.group("AXIOS API");
 //> API With Axios
+// API Key ==> c66eb9e2b42b5d1d179fff7ac34ce71f
+// Request API ==>  https://api.themoviedb.org/3/movie/550?api_key=c66eb9e2b42b5d1d179fff7ac34ce71f
+
+// (A) > Request / petición
+
+const getAllMovies = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular`,
+      {
+        params: {
+          //api_key: `c66eb9e2b42b5d1d179fff7ac34ce71f`, //> Token <
+          language: `es-MX`,
+        },
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjZlYjllMmI0MmI1ZDFkMTc5ZmZmN2FjMzRjZTcxZiIsInN1YiI6IjYxODZlNGYyYTMxM2I4MDA4ZjQ1Y2NkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._SyV7z_TCNXZRmYDiihDspxJTkXT8LFU5MueX4MHDsg`,
+        },
+      }
+    );
+
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getAllMovies();
+
+// (B) >
+//> Intersection Observer <
+let observer = new IntersectionObserver((entries, observer) => {}, {
+  rootMargin: `0px 0px 200px 0px`,
+  threshold: 1.0,
+});
 
 console.groupEnd();
