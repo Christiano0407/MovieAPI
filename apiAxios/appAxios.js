fd8f5c90a91ffcb2e2fb2035c941290bb6200e34
Christiano0407 <placebo0704@gmail.com>
2021-11-21 17:56:49 -0600

    terminal -movies

--- apiAxios/appAxios.js
+++ apiAxios/appAxios.js
@@ -2,6 +2,7 @@ console.group("AXIOS API");
 //> API With Axios
 // API Key ==> c66eb9e2b42b5d1d179fff7ac34ce71f
 // Request API ==>  https://api.themoviedb.org/3/movie/550?api_key=c66eb9e2b42b5d1d179fff7ac34ce71f
+let movies = ` `;
 
 // (A) > Request / petición
 
@@ -15,12 +16,25 @@ const getAllMovies = async () => {
           language: `es-MX`,
         },
         headers: {
+          // Token:
           Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjZlYjllMmI0MmI1ZDFkMTc5ZmZmN2FjMzRjZTcxZiIsInN1YiI6IjYxODZlNGYyYTMxM2I4MDA4ZjQ1Y2NkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._SyV7z_TCNXZRmYDiihDspxJTkXT8LFU5MueX4MHDsg`,
         },
       }
     );
 
     console.log(response);
+
+    //>
+    if (response.status === 200) {
+      response.data.results.forEach((movie) => {
+        movies += ` <div>
+            <img class="poster" src= "https://image.tmdb.org/t/p/w500/${movie.poster_path}">
+            <h3 class="title">${movie.title}</h3>
+            </div> `;
+      });
+    }
+
+    document.getElementById(`container`).innerHTML = movies;
   } catch (error) {
     console.log(error);
   }
