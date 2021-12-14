24d402fdd5d80eb61bb9b961e83f6e672de9c43c
Christiano0407 <placebo0704@gmail.com>
2021-12-14 09:02:21 -0600

    add enlaces and observer

--- scrollSpy/src/appSpy.js
+++ scrollSpy/src/appSpy.js
@@ -2,3 +2,31 @@ const encabezados = document.querySelectorAll(`.contenedor .encabexado`);
 console.log(encabezados);
 const enlaces = document.querySelectorAll(`#enlaces a`);
 console.log(enlaces);
+
+const observer = new IntersectionObserver((entradas, observador) => {
+  entradas.forEach(
+    (entrada) => {
+      if (entrada.isIntersecting) {
+        const id = `#` + entrada.target.id;
+        history.pushState({}, entrada.target.innerText, id);
+
+        enlaces.forEach((enlace) => {
+          enlace.classList.remove(`activo`);
+
+          const href = enlace.attributes.href.nodeValue;
+          if (href === id) {
+            enlace.classList.add(`activo`);
+          }
+        });
+      }
+    },
+    {
+      threshold: 1,
+      rootMargin: `0px 0px -50% 0px`,
+    }
+  );
+
+  encabezados.forEach((encabezado) => {
+    observer.observe(encabezado);
+  });
+});
