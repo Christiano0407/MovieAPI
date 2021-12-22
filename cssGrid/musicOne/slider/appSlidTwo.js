fb080c7a10e727cc65dc0070a2bfb1934d9230d4
Christiano0407 <placebo0704@gmail.com>
2021-12-22 15:36:45 -0600

    swipper

--- cssGrid/musicOne/slider/appSlidTwo.js
+++ cssGrid/musicOne/slider/appSlidTwo.js
@@ -1,1 +1,23 @@
 // Framework Swipper Js
+ var mySwiper = new Swiper(".swiper-container", {
+   // Optional parameters
+   spaceBetween: 5,
+   slidesPerView: 2,
+   loop: true,
+   freeMode: true,
+   loopAdditionalSlides: 5,
+   speed: 500,
+   // Navigation arrows
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+   breakpoints: {
+     // when window width is >= 640px
+     640: {
+       slidesPerView: 5,
+       slidesPerGroup: 5,
+       freeMode: false,
+     },
+   },
+ });
---
+++ cssGrid/musicOne/slider/appTwoSlid.js
@@ -0,0 +1,5 @@
+// Slider Two
+const swiper = new Swiper(`.swiper`, {
+  speed: 400,
+  spaceBetween: 100,
+});
