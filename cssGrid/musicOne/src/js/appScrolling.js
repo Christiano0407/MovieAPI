3d68812b21aee4487723719016dfd7c3c9b3c7a2
Christiano0407 <placebo0704@gmail.com>
2021-12-21 22:27:46 -0600

    add image all

--- cssGrid/musicOne/src/js/appScrolling.js
+++ cssGrid/musicOne/src/js/appScrolling.js
@@ -8,5 +8,8 @@ let disableScroll = false;
 let scrollPos;
 let items = [...document.querySelectorAll(`.slider-item`)];
 console.log(items);
-let image = [...document.querySelectorAll(`.img-div`)];
+let images = [...document.querySelectorAll(`.img-div`)];
 // = Events =
+images.forEach((image, idx) => {
+  image.style.backgroundImage = `url(/src/img/${idx + 1}.jpg)`;
+});
