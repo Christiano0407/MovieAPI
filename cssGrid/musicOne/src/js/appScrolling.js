26483f31e5582bd826494837fe563299120e7ae7
Christiano0407 <placebo0704@gmail.com>
2021-12-21 22:03:22 -0600

    add slider

--- cssGrid/musicOne/src/js/appScrolling.js
+++ cssGrid/musicOne/src/js/appScrolling.js
@@ -1,10 +1,12 @@
 // Scrolling Javascript Animation
 const sliderWrapper = document.getElementById(`sliderWrapper`);
 const slider = document.getElementById(`slider`);
-const sliderItem = document.getElementById(`sliderItem`);
 let clonesWidth;
 let sliderWidth;
 let clones = [];
 let disableScroll = false;
 let scrollPos;
+let items = [...document.querySelectorAll(`.slider-item`)];
+console.log(items);
+let image = [...document.querySelectorAll(`.img-div`)];
 // = Events =
