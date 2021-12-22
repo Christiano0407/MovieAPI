cc964ed78005e96c24e8a21f6495c2728dbb566f
Christiano0407 <placebo0704@gmail.com>
2021-12-21 21:40:29 -0600

    scrolling 1

--- cssGrid/musicOne/src/js/appScrolling.js
+++ cssGrid/musicOne/src/js/appScrolling.js
@@ -1,1 +1,10 @@
 // Scrolling Javascript Animation
+const sliderWrapper = document.getElementById(`sliderWrapper`);
+const slider = document.getElementById(`slider`);
+const sliderItem = document.getElementById(`sliderItem`);
+let clonesWidth;
+let sliderWidth;
+let clones = [];
+let disableScroll = false;
+let scrollPos;
+// = Events =
