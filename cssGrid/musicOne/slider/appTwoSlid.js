b10ce52888387d07aaac41e2845ea86096b0ac58
Christiano0407 <placebo0704@gmail.com>
2021-12-22 16:20:14 -0600

    slider previous final

--- cssGrid/musicOne/slider/appTwoSlid.js
+++ cssGrid/musicOne/slider/appTwoSlid.js
@@ -1,5 +1,27 @@
 // Slider Two
 const swiper = new Swiper(`.swiper`, {
+  // Optional parameters
+  direction: `horizontal`,
+  loop: true,
   speed: 400,
-  spaceBetween: 100,
+
+  slidesPerView: 2,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // If we need pagination
+  /* pagination: { */
+  /*   el: `.swiper-pagination`, */
+  /* }, */
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // And if we need scrollbar
+  /*  scrollbar: { */
+  /*    el: `.swiper-scrollbar`, */
+  /*  }, */
 });
