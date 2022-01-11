a1722909cf1bc87145816eaa4641613e8546a58f
Christiano0407 <placebo0704@gmail.com>
2022-01-11 08:07:17 -0600

    list-Audio

---
+++ jsMusic/src/js/appMusic.js
@@ -0,0 +1,22 @@
+console.group("Music Audio");
+
+// Audio List
+const listAudio = [
+  {
+    name: "Artist 1 - Audio 1",
+    File: "../src/assets/music/audio1.mp3",
+    duration: "0:36",
+  },
+  {
+    name: "Artist 2 - Audio 2",
+    file: "../src/assets/music/audio2.mp3",
+    duratio: "1:36",
+  },
+  {
+    name: "Artist 3 - Audio 3",
+    file: "../src/assets/music/audio3.mp3",
+    duration: "0:30",
+  },
+];
+
+console.groupEnd();
