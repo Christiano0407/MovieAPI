e077f56a1a3e42362bae99d878de044c82c33ab4
Christiano0407 <placebo0704@gmail.com>
2022-01-17 10:48:07 -0600

    nav__list-02

---
+++ awesomeCSS/nav01/appNav01.js
@@ -0,0 +1,7 @@
+console.group("Btn Click Nav");
+function actionToggle() {
+  const drop = document.querySelector(".drop__list--media");
+  drop.classList.toggle("active");
+}
+
+console.groupEnd();
