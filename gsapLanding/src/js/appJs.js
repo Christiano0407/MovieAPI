a1618804839d1a009126ec565a359a4011457479
Christiano0407 <placebo0704@gmail.com>
2022-01-19 22:20:32 -0600

    timeLine_Js

--- gsapLanding/src/js/appJs.js
+++ gsapLanding/src/js/appJs.js
@@ -1,3 +1,16 @@
 console.group("Animation GSAP");
 
+// GSAP Animation ==>
+const tl = new TimelineMax();
+
+// Circle Outer >
+tl.fromTo(".circle__outer", 1, { opacity: "0" }, { opacity: "1" }, 1.5);
+//  Circle Inner
+tl.fromTo(".circle__inner", 1, { opacity: "0" }, { opacity: "1" }, 2.5);
+// Aside
+tl.from(".landing aside", 0.5, { x: "-150" }, 0.5);
+// header
+tl.from(".landing .section .header", 0.8, { y: "-150" }, 1);
+//Features
+
 console.groupEnd();
