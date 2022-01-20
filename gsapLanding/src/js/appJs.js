3b9b4ab570bc3db25617d8ce0ce58c79d353eccd
Christiano0407 <placebo0704@gmail.com>
2022-01-19 22:43:55 -0600

    gsap 01

--- gsapLanding/src/js/appJs.js
+++ gsapLanding/src/js/appJs.js
@@ -12,5 +12,36 @@ tl.from(".landing aside", 0.5, { x: "-150" }, 0.5);
 // header
 tl.from(".landing .section .header", 0.8, { y: "-150" }, 1);
 //Features
+tl.from(
+  ".landing section .content .features .feature h1, .landing section .content .features .feature p",
+  0.5,
+  { y: "100" },
+  2.5
+);
+
+tl.fromTo(
+  ".landing .section .content .features .feature span",
+  0.5,
+  { height: "0" },
+  { height: "10" },
+  3
+);
+
+tl.fromTo(
+  ".landing .section .content .features .feature:nth-child(2) span",
+  0.5,
+  { height: "0" },
+  { height: "50" },
+  3
+);
+// got
+tl.fromTo(
+  ".landing .section .content .theme h2 .wrap",
+  1,
+  { top: "300" },
+  { top: "0" },
+  1
+);
+// Miracle
 
 console.groupEnd();
