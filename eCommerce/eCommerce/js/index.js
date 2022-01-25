cb1d676fbae21dc3786203b4e958943ca94f0280
Christiano0407 <placebo0704@gmail.com>
2022-01-25 00:52:27 -0600

    create__list--js

---
+++ eCommerce/eCommerce/js/index.js
@@ -0,0 +1,81 @@
+const wrapper = document.querySelector(".slider__wrapper");
+// const menuItems
+
+// = Products =
+const products = [
+  {
+    id: 1,
+    title: " Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "/eCommerce/src/assets/img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "/eCommerce/src/assets/img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: " lighgray",
+        img: "/eCommerce/src/assets/img/jordan.png",
+      },
+      {
+        code: "green",
+        img: " /eCommerce/src/assets/img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 134,
+    colors: [
+      {
+        code: "lightgray",
+        img: "/eCommerce/src/assets/img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "/eCommerce/src/assets/img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 125,
+    colors: [
+      {
+        code: "black",
+        img: "/eCommerce/src/assets/img/Crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "/eCommerce/src/assets/img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "hippie",
+    price: 119,
+    colors: [
+      {
+        code: "gray",
+        img: "/eCommerce/src/assets/img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "/eCommerce/src/assets/img/hippie2.png",
+      },
+    ],
+  },
+];
