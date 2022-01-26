a0508520a33bc44185b725257d7f739dc257c781
Christiano0407 <placebo0704@gmail.com>
2022-01-26 15:55:05 -0600

    shoes-previous_final

--- eCommerce/eCommerce/js/index.js
+++ eCommerce/eCommerce/js/index.js
@@ -1,11 +1,13 @@
 const wrapper = document.querySelector(".slider__wrapper");
 // const menuItems
+const menuItems = document.querySelectorAll(".menuItem");
+console.log(menuItems);
 
 // = Products =
 const products = [
   {
     id: 1,
-    title: " Air Force",
+    title: " AIR FORCE",
     price: 119,
     colors: [
       {
@@ -20,7 +22,7 @@ const products = [
   },
   {
     id: 2,
-    title: "Air Jordan",
+    title: "Air JORDAN",
     price: 149,
     colors: [
       {
@@ -35,7 +37,7 @@ const products = [
   },
   {
     id: 3,
-    title: "Blazer",
+    title: "AIR BLAZER",
     price: 134,
     colors: [
       {
@@ -50,7 +52,7 @@ const products = [
   },
   {
     id: 4,
-    title: "Crater",
+    title: "AIR CREATE",
     price: 125,
     colors: [
       {
@@ -65,7 +67,7 @@ const products = [
   },
   {
     id: 5,
-    title: "hippie",
+    title: "AIR WAVES",
     price: 119,
     colors: [
       {
@@ -84,4 +86,48 @@ const products = [
 let choseProducts = products[0];
 
 // Products Current>
-//const currentProductImg = document.querySelector(".")
+const currentProductImg = document.querySelector(".product-img");
+const currentProductTitle = document.querySelector(".product-title");
+const currentProductPrice = document.querySelector(".product-price");
+const currentProductDesc = document.querySelector(".product-desc");
+const currentProductColor = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+// Event MenuItem  and Method===>
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choseProducts = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choseProducts.title;
+    currentProductPrice.textContent = "$" + choseProducts.price;
+    currentProductImg.src = choseProducts.colors[0].img;
+
+    //assing new colors
+    currentProductColor.forEach((color, index) => {
+      color.style.backgroundColor = choseProducts.colors[index].code;
+    });
+  });
+});
+
+currentProductColor.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choseProducts.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
